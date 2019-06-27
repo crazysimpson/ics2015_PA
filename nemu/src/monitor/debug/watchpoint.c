34a582fef4c44ddcc1c4136db900b43bbffa7dcb
tracer-ics2015 <tracer@njuics.org>
2019-06-27 09:44:10 +0800

    >  compile NEMU 141220000 root Linux localhost.localdomain 2.6.32-754.14.2.el6.i686 #1 SMP Tue May 14 20:04:54 UTC 2019 i686 i686 i386 GNU/Linux  09:44:10 up  1:01,  2 users,  load average: 0.08, 0.02, 0.01 5f00f3be1ea8c7c873014f8e46984ad0ca64fc69

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -78,7 +78,7 @@ void delete_wp(unsigned addr){
 
 void print_wp(){
 	WP *tmp = wp_pool;
-	while(tmp<head){
+	while(tmp<=head){
 		printf("watchpoint: %08x,  content: %08x,  current_value: %08x\n", tmp->point, tmp->content, swaddr_read((swaddr_t)tmp->point, 4));
 		tmp = tmp->next;
 	}	
